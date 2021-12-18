14f960aa31c2e525672d944c27b8fc4b92a9490e
Glebexe <53045081+Glebexe@users.noreply.github.com>
2021-12-18 15:17:34 +0300

    complete test

--- task_10/src/com/company/ContractPayments.java
+++ task_10/src/com/company/ContractPayments.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class ContractPayments {
+    public static ContractPayments create() {
+        return new ContractPayments();
+    }
+
+    public int getContractsCount() {
+        return 0;
+    }
 }
