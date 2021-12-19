03aea9adc22b2de36e991024fbd96f0d75d93694
Glebexe <53045081+Glebexe@users.noreply.github.com>
2021-12-19 11:23:43 +0300

    completing tests

--- task_10/src/com/company/Contract.java
+++ task_10/src/com/company/Contract.java
@@ -5,17 +5,24 @@ import java.util.HashMap;
 public class Contract {
     private String date;
     private HashMap<Integer, PaymentDocument> paymentDocuments;
+    private int sumOfAllPayments;
 
     public Contract(String date){
         this.date = date;
         paymentDocuments = new HashMap<>();
+        sumOfAllPayments = 0;
     }
 
     public void registerPaymentDocument(int sum, int number, PaymentDocumentType paymentType, String date){
         paymentDocuments.put(number,new PaymentDocument(sum,paymentType,date));
+        sumOfAllPayments += sum;
     }
 
     public int getPaymentDocumentsCount() {
         return paymentDocuments.size();
+    }
+
+    public int getAmountOfPayments() {
+        return sumOfAllPayments;
     }
 }
